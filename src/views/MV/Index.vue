6c7129c632e02b23a37dedf074172c8ed35b8f31
szq <814471424@qq.com>
2023-04-27 16:45:08 +0800

    update

--- src/views/MV/Index.vue
+++ src/views/MV/Index.vue
@@ -30,7 +30,7 @@ import CommentList from '../../components/Common/CommentList.vue'
 
 
 let mvid = ref(router.currentRoute.value.params['id'] as string)
-let mvUrl: Ref<string | null> = ref(null);
+let mvUrl: Ref<string> = ref('');
 let active = ref(0)
 const limit = 20;
 let page = ref(1);
@@ -76,7 +76,7 @@ onMounted(() => {
 }
 
 .mv-body .other-mv {
-  width: 200px;
+  width: 230px;
   background-color: rgb(45, 65, 65);
 }
 
